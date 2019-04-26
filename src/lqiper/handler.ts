5694f354d67d7189ac60f188f9ebc1c683156935
Aleksi Pekkala <aleksi.v.a.pekkala@student.jyu.fi>
2019-04-26 09:32:33 +0300

    Replace SQIPer functions with LQIPer

--- src/lqiper/handler.ts
+++ src/lqiper/handler.ts
@@ -1,16 +1,14 @@
 import { Storage } from '@google-cloud/storage'
-import fs from 'fs'
 import os from 'os'
 import path from 'path'
-import { promisify } from 'util'
+import lqip from '../lqip'
 import { CloudStorageObject, Context } from '../model'
-import sqip from '../sqip'
 
 const gcs = new Storage()
 const BUCKET_THUMBNAILS = gcs.bucket('imgmgr-thumbnails')
 const TMP_DIR = os.tmpdir()
 
-export async function sqiper(data: CloudStorageObject, context: Context) {
+export async function lqiper(data: CloudStorageObject, context: Context) {
   const fileName = `${new Date().toISOString()}-${data.name}`
   const tmpFilePath = path.join(TMP_DIR, fileName)
   await gcs
@@ -18,11 +16,11 @@ export async function sqiper(data: CloudStorageObject, context: Context) {
     .file(data.name)
     .download({ destination: tmpFilePath })
 
-  const { final_svg } = sqip(tmpFilePath)
-  const sqipName = `sqip-${path.parse(fileName).name}.svg`
-  const sqipPath = path.join(TMP_DIR, sqipName)
-  await promisify(fs.writeFile)(sqipPath, final_svg)
-  return BUCKET_THUMBNAILS.upload(sqipPath, {
-    destination: sqipName,
+  const lqipName = `lqip-${fileName}`
+  const lqipPath = path.join(TMP_DIR, lqipName)
+  return lqip(tmpFilePath, lqipPath).then(() => {
+    return BUCKET_THUMBNAILS.upload(lqipPath, {
+      destination: lqipName,
+    })
   })
 }
--- src/sqip.ts
+++
@@ -1,8 +0,0 @@
-import sqip from 'sqip'
-
-export default function makeSqip(inputPath: string, numberOfPrimitives = 5) {
-  return sqip({
-    filename: inputPath,
-    numberOfPrimitives,
-  })
-}
